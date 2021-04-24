c3309e4c15c3e035721794b0ebb4821a7408ed01
Josh Chernoff <jchernoff@morphic.pro>
2021-04-24 16:07:25 -0700

    Fix typo

--- mix.exs
+++ mix.exs
@@ -75,7 +75,7 @@ defmodule LeydenJar.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate", "test"],
-      t: ["ecto.create --quiet", "ecto.migrate", "coveralls.html", "credo --strict"
+      t: ["ecto.create --quiet", "ecto.migrate", "coveralls.html", "credo --strict"]
     ]
   end
 end
