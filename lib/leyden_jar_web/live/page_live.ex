630124be140e3c3721a4f5d0118cdde8d2c58fda
Josh Chernoff <jchernoff@morphic.pro>
2021-04-24 23:06:09 -0700

    Update styles

--- lib/leyden_jar_web/live/page_live.ex
+++ lib/leyden_jar_web/live/page_live.ex
@@ -1,39 +1,44 @@
 defmodule LeydenJarWeb.PageLive do
   use LeydenJarWeb, :live_view
+  alias LeydenJar.Accounts
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
-  end
-
-  @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
-  end
-
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
+  def mount(_params, session, socket) do
+    if user = session["user_token"] && Accounts.get_user_by_session_token(session["user_token"]) do
+      {:ok, assign(socket, current_user: user)}
+    else
+      {:ok, assign(socket, current_user: nil)}
     end
   end
 
-  defp search(query) do
-    if not LeydenJarWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
+  # @impl true
+  # def handle_event("suggest", %{"q" => query}, socket) do
+  #   {:noreply, assign(socket, results: search(query), query: query)}
+  # end
 
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
-  end
+  # @impl true
+  # def handle_event("search", %{"q" => query}, socket) do
+  #   case search(query) do
+  #     %{^query => vsn} ->
+  #       {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
+
+  #     _ ->
+  #       {:noreply,
+  #        socket
+  #        |> put_flash(:error, "No dependencies found matching \"#{query}\"")
+  #        |> assign(results: %{}, query: query)}
+  #   end
+  # end
+
+  # defp search(query) do
+  #   if not LeydenJarWeb.Endpoint.config(:code_reloader) do
+  #     raise "action disabled when not in development"
+  #   end
+
+  #   for {app, desc, vsn} <- Application.started_applications(),
+  #       app = to_string(app),
+  #       String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
+  #       into: %{},
+  #       do: {app, vsn}
+  # end
 end
